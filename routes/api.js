59b55c348ef12e4e1239fc2b6cf5e7ffa7ed69cd
Esiena Ekwofia <eekwofia@gmail.com>
2021-03-10 19:27:20 -0500

    setting up api routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,13 @@
 const db = require("../models");
 const router = require("express").Router();
 
+//display all workouts
+app.get()
 
+// route to add a workout to tracker
+app.put()
 
+// route to create a new exercise
+app.post()
 
 module.exports = router;
