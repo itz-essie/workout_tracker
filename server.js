b47b0587ca2cc348d5434a8aeb15268eefb1f082
Esiena Ekwofia <eekwofia@gmail.com>
2021-03-13 16:19:24 -0500

    fixing issues

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 3006;
 
 const app = express();
 
-app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.use(logger("dev"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutsdb", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 
