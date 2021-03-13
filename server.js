50793e14dcdd090f8b289023c235b2e8182d597e
Esiena Ekwofia <eekwofia@gmail.com>
2021-03-13 15:24:33 -0500

    deploying

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutsdb", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutsdb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 
 app.use(require("./routes/api.js"));
